d52cede2963c56dcfe9a08fa42b6828a23770b9d
cark in real life <111182808+yeacark@users.noreply.github.com>
2024-10-17 18:14:49 +0200

    guess who's back?

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import nextcord, NextcordUtils, random
 from nextcord.ext import commands
 
-PREFIX = ['-']
-
 intents = nextcord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+
+bot = commands.Bot(command_prefix="-", intents=intents)
+music = NextcordUtils.Music()
 
 bot.remove_command('help')
 
@@ -16,8 +16,7 @@ async def on_ready():
     await bot.change_presence(activity=nextcord.Game(name="-help"))
 
 # Lista variabili
-FOTO = ("https://www.chenews.it/wp-content/uploads/2020/08/Gerry-Scotti-2-650x472.jpg",
-"https://64.media.tumblr.com/a573941f1a64ff30e1f97236e58d42b6/tumblr_plcftg3xrA1tlsda4o1_500.gifv",
+foto = ("https://www.chenews.it/wp-content/uploads/2020/08/Gerry-Scotti-2-650x472.jpg",
 "https://www.donnamoderna.com/content/uploads/2020/10/GettyImages-1142898643.jpg",
 "https://www.youmovies.it/wp-content/uploads/2022/08/Gerry-Scotti-1.jpg",
 "https://www.cinematographe.it/wp-content/uploads/2022/08/IMG_20220807_095932.jpg",
@@ -27,10 +26,9 @@ FOTO = ("https://www.chenews.it/wp-content/uploads/2020/08/Gerry-Scotti-2-650x47
 "https://www.cinematographe.it/wp-content/uploads/2022/08/IMG_20220807_095932.jpg",
 "https://cdn.gelestatic.it/kataweb/tvzap/2016/01/foto_gerry_scottiok.jpg",
 "https://www.gedistatic.it/content/gedi/img/huffingtonpost/2018/01/27/105244273-1be2e529-20d3-4532-8eb3-c6ca21de47cc.jpeg",
-"https://st.ilfattoquotidiano.it/wp-content/uploads/2018/11/06/scotti-905.jpg",
-"https://img.corr.it/images/2021/09/11/171220387-cff766ae-2851-45fc-a105-11979fd5e47d.jpg")
+"https://st.ilfattoquotidiano.it/wp-content/uploads/2018/11/06/scotti-905.jpg")
 
-VIDS = ("https://www.youtube.com/watch?v=0nnSNO7WdDE",
+vids = ("https://www.youtube.com/watch?v=0nnSNO7WdDE",
 "https://www.youtube.com/watch?v=A_KW0D-n5wM",
 "https://www.youtube.com/watch?v=ULZYNV4jlMQ",
 "https://www.youtube.com/watch?v=p0LHE8yRyvg",
@@ -54,10 +52,7 @@ VIDS = ("https://www.youtube.com/watch?v=0nnSNO7WdDE",
 "https://www.youtube.com/watch?v=hQ7lLOg63rg")
 
 berlusco = ("https://www.artribune.com/wp-content/uploads/2011/11/227.jpg",
-"https://www.tag24.it/wp-content/uploads/2022/03/universitas-libertatis-home.jpg",
-"https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt7df1cc806c3fb76b/60de8432ddcd520eeb9509e4/0c142578c4918e368e21b1e4da94ee1ed2675a7e.jpg",
 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiZ5hTuH6VYecmq-9yQ4C6jtDtSU57_jDVPFt2kEvhpA&s",
-"https://www.ciociariaoggi.it/download/img/full/89791_6wzswjt.jpg",
 "https://abruzzoweb.it/wp-content/uploads/2020/09/silvioberlusconinw345.jpg",
 "https://www.raiplay.it/cropgd/1080x720/dl/img/2018/02/15189870859818396449.png",
 "https://www.bdtorino.net/files/lmDmONNNxzuCoNQVRz91-07dona-xlarge1-jpg.jpg",
@@ -74,52 +69,39 @@ salvoni=("https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Matteo_Salvi
 
 @bot.command(aliases = ["aiuto"])
 async def help(ctx):
-    help1 = nextcord.Embed(color=nextcord.Color.from_rgb(248, 117, 255)).add_field(name=f"Lista comandi - Pagina 1", value=f"**.ciao / .hi** \
-    -  Ti Saluta!\n\n**.gerry / .g**  \
-    -  Manda una foto di Gerry Scotti!\n\n**.ripeti / .r** \
-    -  Ripete un qualsiasi messaggio scritto da te\n\n**.info** \
-    -  Mostra informazioni sul creatore del bot\n\n**.ziopera / .zp** \
-    -  Zio pera...\n\n**.frittomisto / .splash** \
-    -  SPLASHHH\n\n**.turiipipip** \
-    -  Manda turi ip ip ip in chat\n\n**.wenomechainsama** \
-    -  Manda wenomechainsama in chat")
-    help2 = nextcord.Embed(color=nextcord.Color.from_rgb(248, 117, 255)).add_field(name="Lista comandi - Pagina 2", value="**.molise** \
-    - Manda una foto del Molise\n\n**.xijinping / .xi** \
-    -  Xi Jinping Winnie The Pooh fa ridere.\n\n**.taiwan** \
-    -  Il Taiwan esiste.\n\n**.carkvids / .cark** \
-    -  Manda il link di un video di cark\n\n**.berlusconi / .b** \
-    -  Manda una foto carina di Berlusconi\n\n**.porco** \
-    -  Manda la foto di un porco\n\n **.salvini** \
+    help1 = nextcord.Embed(color=nextcord.Color.from_rgb(248, 117, 255)).add_field(name=f"Lista comandi - Pagina 1", value=f"**-gerry / -g**  \
+    -  Manda una foto di Gerry Scotti!\n\n**-info** \
+    -  Mostra informazioni sul creatore del bot\n\n**-ziopera / -zp** \
+    -  Zio pera...\n\n**-frittomisto** \
+    -  SPLASHHH\n\n**-turiipipip** \
+    -  Manda turi ip ip ip in chat\n\n**-wenomechainsama** \
+    -  Manda wenomechainsama in chat\n\n**-salvini** \
     -  Manda una foto carina di Matteo Salvini")
+    help2 = nextcord.Embed(color=nextcord.Color.from_rgb(248, 117, 255)).add_field(name="Lista comandi - Pagina 2", value="**-molise** \
+    - Manda una foto del Molise\n\n**-xijinping / -xi** \
+    -  Xi Jinping Winnie The Pooh fa ridere.\n\n**-taiwan** \
+    -  Il Taiwan esiste.\n\n**-carkvids / -cark** \
+    -  Manda il link di un video di cark\n\n**-berlusconi / -b** \
+    -  Manda una foto carina di Berlusconi\n\n**-porco** \
+    -  Manda la foto di un porco")
     help1.set_thumbnail(url="https://static.wikia.nocookie.net/youtube/images/1/1a/Carkyboi.png/revision/latest/scale-to-width-down/250?cb=20200924235749")
     help2.set_thumbnail(url="https://static.wikia.nocookie.net/youtube/images/1/1a/Carkyboi.png/revision/latest/scale-to-width-down/250?cb=20200924235749")
     paginator = NextcordUtils.Pagination.AutoEmbedPaginator(ctx)
     embeds = [help1, help2]
     await paginator.run(embeds)
 
-@bot.command(aliases = ["hi"])
-# Comando abbastanza inutile
-async def ciao(ctx):
-    await ctx.send("Ciao, " + ctx.author.mention + "! :wave:")
-
 @bot.command(aliases = ["g"])
 # Manda una foto di Gerry Scotti
 async def gerry(ctx):    
-    gerry=nextcord.Embed(title="Ecco una foto di Gerry per te ;)", color=nextcord.Color.from_rgb(248, 117, 255))
-    gerry.set_image(url=f"{random.choice(FOTO)}")
+    gerry=nextcord.Embed(title="Ecco una foto di Gerry per te :3", color=nextcord.Color.from_rgb(248, 117, 255))
+    gerry.set_image(url=f"{random.choice(foto)}")
     await ctx.send(embed=gerry)
-
-
-@bot.command(aliases = ["r"])
-# Ripete un qualsiasi messaggio
-async def ripeti(ctx, *, arg):
-    await ctx.send(arg)
 
 @bot.command()
 # Informazioni sul creatore del bot
 async def info(ctx):
-  info = nextcord.Embed(title="Info sul creatore del bot", description="""Bot creato da cark con la biro!!1!
-  ~~Non~~ so una pippa di Python quindi accontentatevi del troiaio che è venuto fuori ;).
+  info = nextcord.Embed(title="Info sul creatore del bot", description="""Bot creato da yeacark:bangbang:\n
+  ~~Non~~ so una pippa di Python quindi accontentatevi del troiaio che è venuto fuori.
   Ovvio aiuto di Rice7th che sennò qua cark fa suicidare i contributor che vedono sto codice :rolling_eyes:
   https://yeacark.github.io/""", color=nextcord.Color.from_rgb(248, 117, 255))
   info.set_thumbnail(url="https://static.wikia.nocookie.net/youtube/images/1/1a/Carkyboi.png/revision/latest/scale-to-width-down/250?cb=20200924235749")
@@ -133,7 +115,7 @@ async def ziopera(ctx):
     ziopera.set_image(url="attachment://ziopera.gif")
     await ctx.send(file=zpzp, embed=ziopera)
 
-@bot.command(aliases=["splash"])
+@bot.command()
 # Splashhh
 async def frittomisto(ctx):
     file = nextcord.File("audio/frittomisto.mp3")
@@ -175,16 +157,16 @@ async def taiwan(ctx):
 @bot.command(aliases=["cark"])
 # Invia il link di un video di cark!
 async def carkvids(ctx):
-    await ctx.send(f"{random.choice(VIDS)}")
+    await ctx.send(f"{random.choice(vids)}")
 
 @bot.command(aliases=["b"])
 # Manda una foto carina di Berlusconi
 async def berlusconi(ctx):
-    berlusconi=nextcord.Embed(title="Ecco una foto carina di Berlusconi per te :)", color=nextcord.Color.from_rgb(248, 117, 255))
+    berlusconi=nextcord.Embed(title="Ecco una foto carina di Berlusconi per te", color=nextcord.Color.from_rgb(248, 117, 255))
     berlusconi.set_image(url=f"{random.choice(berlusco)}")
     await ctx.send(embed=berlusconi)
 
-@bot.command()
+@bot.command(aliases=["p"])
 # Porco
 async def porco(ctx):
         porco=nextcord.Embed(title="Porco", color=nextcord.Color.from_rgb(248, 117, 255))
@@ -194,9 +176,44 @@ async def porco(ctx):
 @bot.command(aliases=["s"])
 # Manda una foto carina di Salvini
 async def salvini(ctx):
-    salvini=nextcord.Embed(title="Ecco una foto carina di Salvini ;)", color=nextcord.Color.from_rgb(248, 117, 255))
+    salvini=nextcord.Embed(title="Ecco una foto carina di Salvini", color=nextcord.Color.from_rgb(248, 117, 255))
     salvini.set_image(url=f"{random.choice(salvoni)}")
     await ctx.send(embed=salvini)
+
+@bot.command()
+# Fa partire Fritto Misto in chat vocale
+async def splash(ctx, *, url):
+    # Controlla se l'utente è in un canale vocale
+    if ctx.author.voice is None:
+        await ctx.send("Devi essere in un canale vocale per usare questo comando.")
+    voice_channel = ctx.author.voice.channel
+    # Controlla se il bot è già connesso a un canale vocale
+    if ctx.voice_client is None:
+        await voice_channel.connect()
+    elif ctx.voice_client.channel != voice_channel:
+        await ctx.voice_client.move_to(voice_channel)
+    voice_client = ctx.voice_client
+    # Carica e riproduce l'audio
+    source = nextcord.FFmpegPCMAudio(url)
+    if not voice_client.is_playing():
+        voice_client.play(source)
+        await ctx.send(f"In riproduzione FRITTO MISTO")
+    else:
+        song = await voice_client.queue(url, search=True)
+        await ctx.send(f"Accodato {song.name}")
+
+@bot.command(aliases=["fottiti"])
+# Se leva dai maroni
+async def esci(ctx):
+    await ctx.voice_client.disconnect()
+    await ctx.send("Esiliato.")
+
+@bot.command(aliases=["zitto"])
+# TASI
+async def muto(ctx):
+    player = music.get_player(guild_id=ctx.guild.id)
+    await player.stop()
+    await ctx.send("Castigato.")
 
 # Il tuo token va qui
 bot.run("
